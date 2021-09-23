921c2ac67e672805a16f023bc46bdcdcbcec817d
dingming <dingming@hz-health.cn>
2021-09-23 16:37:46 +0800

    useRef

--- src/Example8/Example8.js
+++ src/Example8/Example8.js
@@ -1,16 +1,29 @@
-import React, { useRef} from 'react';
+import React, { useRef ,useState,useEffect } from 'react';
 function Example8(){
     const inputEl = useRef(null)
     const onButtonClick=()=>{ 
         inputEl.current.value="Hello ,JSPang"
-        console.log(inputEl) //输出获取到的DOM节点
+        console.log(inputEl)
     }
+    //-----------关键代码--------start
+    const [text, setText] = useState('jspang')
+    const textRef = useRef()
+
+    useEffect(()=>{
+        textRef.current = text;
+        console.log('textRef.current:', textRef.current)
+    })
+    //----------关键代码--------------end
     return (
         <>
             {/*保存input的ref到inputEl */}
             <input ref={inputEl} type="text"/>
             <button onClick = {onButtonClick}>在input上展示文字</button>
+            <br/>
+            <br/>
+            <input value={text} onChange={(e)=>{setText(e.target.value)}} />
         </>
     )
 }
+
 export default Example8
