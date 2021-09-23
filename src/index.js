848b9767d8ef555b604604a4fb598d78a7c8de3c
dingming <dingming@hz-health.cn>
2021-09-23 16:09:41 +0800

    useMemo完结

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Example6/Example6'
+import App from './Example7/Example7'
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
