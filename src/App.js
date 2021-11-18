949885a403cd7a5c4176ad8680a83d6185b12226
dingming <dingming@hz-health.cn>
2021-11-18 21:03:11 +0800

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React,{useReducer} from "react";
-function Example() {
-  function reducer(state,action) {
-    switch(action.type){
-      case 'increment':
-        return {...state,counter:state.counter+1};
-      case 'decrement':
-        return {...state,counter:state.counter-1};
-      default:
-        return state;
-    }
-  }
-  const [state,dispatch] = useReducer(reducer,{counter:0})
+import React, { useState ,useCallback} from "react";
+// 子组件
+function Childs(props) {
+  console.log("子组件渲染了");
   return (
-    <div>
-      <h2>Home当前计数:{state.counter}</h2>
-      <button onClick={e=>dispatch({type:'increment'})}>增加</button>
-      <button onClick={e=>dispatch({type:'decrement'})}>减少</button>
+    <>
+      <button onClick={props.onClick}>改标题</button>
+      <h1>{props.name}</h1>
+    </>
+  );
+}
+const Child = React.memo(Childs);
+export default function App() {
+  const [title, setTitle] = useState("这是一个 title");
+  const [subtitle, setSubtitle] = useState("我是一个副标题");
+  const [c,setC] = useState(0)
+  const [d,setD] = useState(0)
+  const callback = () => {
+    setTitle("标题改变了aaa");
+  };
+  let memoFn = useCallback(callback,[c])
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <h2>{c}</h2>
+      <h2>{d}</h2>
+      
+      <button onClick={() => setC(c=>c+1)}>add</button>
+      <button onClick={() => setD(d=>d+1)}>add</button>
+      <button onClick={() => setSubtitle("副标题改变了")}>改副标题</button>
+      <Child onClick={memoFn} name="桃桃" />
     </div>
   );
 }
-export default Example;
