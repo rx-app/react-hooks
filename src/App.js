9a58816e21fc521e58e89472ea3a0a9ba2335694
dingming <dingming@hz-health.cn>
2021-11-18 10:37:38 +0800

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React,{useState} from "react";
+import React,{useReducer} from "react";
 function Example() {
-  const [count, setCount] = useState(0);
+  function reducer(state,action) {
+    switch(action.type){
+      case 'increment':
+        return {...state,counter:state.counter+1};
+      case 'decrement':
+        return {...state,counter:state.counter-1};
+      default:
+        return state;
+    }
+  }
+  const [state,dispatch] = useReducer(reducer,{counter:0})
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h2>Home当前计数:{state.counter}</h2>
+      <button onClick={e=>dispatch({type:'increment'})}>增加</button>
+      <button onClick={e=>dispatch({type:'decrement'})}>减少</button>
     </div>
   );
 }
