774c1d6ff61d38530df0892ac4cf20f71b32be26
dingming <dingming@hz-health.cn>
2021-11-19 16:43:11 +0800

    react.memo缓存纯函数组件

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-import React, { Component} from "react";
-import './app.css'
+import React, { useState } from "react";
 // 子组件
-export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      count:0
-    }
-  }
-  componentDidUpdate(){
-    
-    console.log(this.state.count)
-  }
-  componentDidMount(){
-    document.body.addEventListener('click', this.changeVal, false);
-  }
-  changeVal = () => {
-      this.setState({
-        count: 3
-      })
-      console.log(this.state.count) // 3 
-      //这个地方会log两次3，说明事件被绑定了2次，这个怎么理解呢？？？
-  }
-  changeCount(){
-    let obj = {
-        a: 100,
-        b: [100, 200, 300],
-        c: {
-          x: 10
-        },
-        d: /^\d+$/,
-        e: () =>{
-          return 1
-        },
-        f:3,
-        g:undefined,
-        h:new Date()
-    }
-    let f = JSON.parse( JSON.stringify(obj) )
-    console.log(f)
-  }
-  render(){
-    return (
-      <div className="dv">
-        <h1>当前计数:{this.state.count}</h1>
-        <button onClick={()=>{ this.changeCount() }}>改变数字</button>
-      </div>
-    )
-  }
+function Childs(props) {
+  console.log("子组件渲染了");
+  return (
+    <>
+      <button onClick={props.onClick}>改标题</button>
+      <h1>{props.name}</h1>
+    </>
+  );
+}
+// const Child = React.memo(Childs);
+
+
+export default function App() {
+  const [title, setTitle] = useState("这是一个 title");
+  const [subtitle, setSubtitle] = useState("我是一个副标题");
+  const callback = () => {
+    setTitle("标题改变了");
+  };
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <button onClick={() => setSubtitle("副标题改变了")}>改副标题</button>
+      <Childs  />
+      {/* 如果不使用react.memo 这个组件即使不接受任何props，只要父组件重新渲染，这个组件就会自动渲染 */}
+    </div>
+  );
 }
