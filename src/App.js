8d1e4230ad8bc14cb259c774a4df7fd26b06bf7c
dingming <dingming@hz-health.cn>
2021-11-19 16:25:32 +0800

    深拷贝

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+import './app.css'
 // 子组件
 export default class App extends Component {
   constructor(props){
@@ -22,28 +23,29 @@ export default class App extends Component {
       //这个地方会log两次3，说明事件被绑定了2次，这个怎么理解呢？？？
   }
   changeCount(){
-    // 点击后count 为100
-    for ( let i = 0; i < 100; i++ ) {
-        this.setState( (prev)=>{
-          return {
-            count:prev.count+1
-          }
-        } );
+    let obj = {
+        a: 100,
+        b: [100, 200, 300],
+        c: {
+          x: 10
+        },
+        d: /^\d+$/,
+        e: () =>{
+          return 1
+        },
+        f:3,
+        g:undefined,
+        h:new Date()
     }
-    
-    // 点击后count 为1
-    // for ( let i = 0; i < 100; i++ ) {
-    //     this.setState( { count: this.state.count + 1 } );
-    // }
-    
-
+    let f = JSON.parse( JSON.stringify(obj) )
+    console.log(f)
   }
   render(){
     return (
-      <>
+      <div className="dv">
         <h1>当前计数:{this.state.count}</h1>
         <button onClick={()=>{ this.changeCount() }}>改变数字</button>
-      </>
+      </div>
     )
   }
 }
