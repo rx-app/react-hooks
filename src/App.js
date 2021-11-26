6101efeaf8d1bd72daa2f0a4718b6486a9e95d7a
dingming <dingming@hz-health.cn>
2021-11-26 16:20:51 +0800

    性能问题

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React,{useState} from "react";
-function Example() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
+
+import React , { useState } from 'react'
+/* 自定义hooks 用于格式化数组将小写转成大写 */
+function useFormatList(list){
+  let res = list.map(item=>{
+    console.log(1111)
+    return item.toUpperCase()
+  })
+  return res
 }
-export default Example;
+/* 父组件传过来的list = [ 'aaa' , 'bbb' , 'ccc'  ] */
+function App({ list }){
+  const [ number ,setNumber ] = useState(0)
+  const newList = useFormatList(list)
+  return <div>
+    <div className="list" >
+      { newList.map(item=><div key={item} >{ item }</div>) }
+    </div>
+    <div className="number" >
+        <div>{ number }</div>
+        <button onClick={()=> setNumber(number + 1) } >add</button>
+    </div>
+  </div>
+}
+export default App
+
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  list ={ [ 'aaa' , 'bbb' , 'ccc'  ] } />
   </React.StrictMode>,
   document.getElementById('root')
 );
